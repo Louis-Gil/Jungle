9108cc75426e7d010f794d6dac205e63346083ef
Gilinsik <lotus0110@nate.com>
2022-10-14 23:01:24 +0900

    1014

---
+++ 04/07 가장긴증가하는부분수열.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+number = int(input())
+list_figure = list(map(int, input().split()))
+dp = [1] * number
+
+for i in range(1, number):
+    for j in range(i):
+        if list_figure[i] > list_figure[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
