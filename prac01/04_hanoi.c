a7bb0c5ef1816f05d3824d43c51fca891e9f7a1d
Gilin3000 <lotus0110@nate.com>
2022-10-21 13:07:24 +0000

    1021 Team M

---
+++ prac01/04_hanoi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void HanoiTower(int n, char from, char mid, char to)
+{
+    if (n==1)
+    {
+        printf("원반 %d를 %c에서 %c로 이동\n", n, from, to);
+        return;
+    }
+    HanoiTower(n-1, from, to, mid);
+    printf("원반 %d를 %c에서 %c로 이동\n", n, from, to);
+    HanoiTower(n-1, mid, from, to);
+}
+
+void main()
+{
+    HanoiTower(3, 'A', 'B', 'C');
+}
