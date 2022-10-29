ccb56d234694dac5ef1df843588a4561f1d6e36c
Gilin3000 <lotus0110@nate.com>
2022-10-29 06:15:10 +0000

    54point

--- mm.c
+++ mm.c
@@ -37,35 +37,50 @@ team_t team = {
 /* Basic constants and macros */
 #define WSIZE 4             /* Word and header/footer size (bytes) */
 #define DSIZE 8             /* Double word size (bytes) */
-#define CHUNKSIZE (1 << 12) /* Extend heap by this amount (bytes) */
+#define CHUNKSIZE (1 << 12) /* Extend heap by this amount (bytes) 처음 4kb*/
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
-/* Pack a size and allocated bit into a word */
+/* Pack a size and allocated bit into a word
+사이즈와 할당여부 패킹 -> 헤더 데이터 */
 #define PACK(size, alloc) ((size) | (alloc))
-/* Read and write a word at address p */
+/* Read and write a word at address p 
+p가 참조하는 워드 리턴 / p에 워드 저장 */
 #define GET(p) (*(unsigned int *)(p))
 #define PUT(p, val) (*(unsigned int *)(p) = (val))
-/* Read the size and allocated fields from address p */
+/* Read the size and allocated fields from address p 
+헤더에서 사이즈만 리턴 / 할당여부만 리턴 */
 #define GET_SIZE(p) (GET(p) & ~0x7)
 #define GET_ALLOC(p) (GET(p) & 0x1)
-/* Given block ptr bp, compute address of its header and footer */
+/* Given block ptr bp, compute address of its header and footer 
+헤더와 풋터를 가리키는 포인터 리턴 */
 #define HDRP(bp) ((char *)(bp)-WSIZE)
 #define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
-/* Given block ptr bp, compute address of next and previous blocks */
+/* Given block ptr bp, compute address of next and previous blocks 
+다음과 이전 블록 포인터 리턴 */
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp)-WSIZE)))
-#define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE(((char *)(bp)-DSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp)-DSIZE)))
 
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 
-/* rounds up to the nearest multiple of ALIGNMENT */
+/* rounds up to the nearest multiple of ALIGNMENT 
+사이즈를 올림하여 ALIGNMENT로 함 */
 #define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+
+static void *extend_heap(size_t words);
+static void *coalesce(void *bp);
+static void *find_fit(size_t asize);
+static void place(void *bp, size_t asize);
+
+static char *heap_listp;
+
+
 /*
  * mm_init - initialize the malloc package.
- 최초 힙영역 할당
- 문제가 있으면 -1, 없으면 0
+ 최초 힙영역 할당  문제가 있으면 -1, 없으면 0
+ 1. padding w, 2~3. prolog (head, foot), 4. epilogue (head)
  */
 int mm_init(void)
 {
@@ -106,7 +121,7 @@ static void *extend_heap(size_t words)
 void *mm_malloc(size_t size)
 {
     size_t asize;
-    size_t extendsize;
+    size_t extendsize; 
     char *bp;
 
     if (size == 0)
@@ -122,7 +137,7 @@ void *mm_malloc(size_t size)
         place(bp, asize);
         return bp;
     }
-
+    // 최소 4kb씩 늘림
     extendsize = MAX(asize, CHUNKSIZE);
     if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
         return NULL;
@@ -130,44 +145,60 @@ void *mm_malloc(size_t size)
     return bp;
 }
 
-/*
- * mm_free - Freeing a block does nothing.
- ptr이 가리키는 블록을 개방
- malloc 또는 remalloc에 의해 생긴 포인터만 반환하는 것을 보장
- */
-void mm_free(void *bp)
+static void *find_fit(size_t asize)
 {
-    size_t size = GET_SIZE(HDRP(bp));
+    void *bp;
 
-    PUT(HDRP(bp), PACK(size, 0));
-    PUT(FTRP(bp), PACK(size, 0));
-    coalesce(bp);
+    for(bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp))
+    {
+        if(!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))){
+            return bp;
+        }
+    }
+    return NULL;
+}
+
+static void place(void *bp, size_t asize)
+{
+    size_t csize = GET_SIZE(HDRP(bp));
+
+    if ((csize - asize) >= (2*DSIZE)){
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+        bp = NEXT_BLKP(bp);
+        PUT(HDRP(bp), PACK(csize-asize, 0));
+        PUT(FTRP(bp), PACK(csize-asize, 0));
+    }
+    else{
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+    }
 }
 
 static void *coalesce(void *bp)
 {
     size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
-    size_t size = GET_SIZE(HEDRP(bp));
-
-    if (prev_alloc && next_alloc)
+    size_t size = GET_SIZE(HDRP(bp));
+    
+    if (prev_alloc && next_alloc) // 앞뒤 할당되어있으면 반환
     {
         return bp;
     }
-    else if (prev_alloc && !next_alloc)
+    else if (prev_alloc && !next_alloc) // 앞이 할당 뒤가 비었음
     {
         size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
         PUT(HDRP(bp), PACK(size, 0));
         PUT(FTRP(bp), PACK(size, 0));
     }
-    else if (!prev_alloc && next_alloc)
+    else if (!prev_alloc && next_alloc) // 뒤가 할당 앞이 비었음
     {
         size += GET_SIZE(HDRP(PREV_BLKP(bp)));
         PUT(FTRP(bp), PACK(size, 0));
         PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
     }
-    else
+    else // 앞뒤 할당 안되어있음
     {
         size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
                 GET_SIZE(FTRP(NEXT_BLKP(bp)));
@@ -192,12 +223,48 @@ void *mm_realloc(void *ptr, size_t size)
     size_t copySize;
 
     newptr = mm_malloc(size);
-    if (newptr == NULL)
-        return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+    if (newptr == NULL){
+        mm_free(ptr);
+        return ;
+        }
+    if (ptr == NULL)//*ptr? ptr
+    {
+        return newptr;
+    }
+    // copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+    copySize = GET_SIZE(HDRP(ptr));
     if (size < copySize)
         copySize = size;
+
     memcpy(newptr, oldptr, copySize);
     mm_free(oldptr);
     return newptr;
+
+    // void *oldptr = ptr;
+    // void *newptr;
+    // size_t copySize;
+
+    // newptr = mm_malloc(size);
+    // if (newptr == NULL)
+    //     return NULL;
+    // copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+    // if (size < copySize)
+    //     copySize = size;
+    // memcpy(newptr, oldptr, copySize);
+    // mm_free(oldptr);
+    // return newptr;
+}
+
+/*
+ * mm_free - Freeing a block does nothing.
+ ptr이 가리키는 블록을 개방
+ malloc 또는 remalloc에 의해 생긴 포인터만 반환하는 것을 보장
+ */
+void mm_free(void *bp)
+{
+    size_t size = GET_SIZE(HDRP(bp));
+
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    coalesce(bp);
 }
