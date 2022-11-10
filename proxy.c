9f47e848bd60e15e6bfd1a6f7417933cbad4d875
Gilin3000 <lotus0110@nate.com>
2022-11-10 00:04:41 +0000

    proxy cache PV추가

--- proxy.c
+++ proxy.c
@@ -22,15 +22,17 @@ void* thread(void* vargp);
 
 sbuf_t sbuf; //
 static CacheList* cacheList;
+static sem_t mutex;
 
 int main(int argc, char **argv)
 {
-    int listenfd, connfd; // 듣기소켓, 연결소켓 받아오기
+    int listenfd, connfd; // 듣기 식별자, 연결식별자
     char hostname[MAXLINE], port[MAXLINE];
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
     pthread_t tid;
     cacheList = initCache();
+    Sem_init(&mutex, 0, 1);
     if (argc != 2) // 에러검출
     {
         fprintf(stderr, "usage: %s <port>\n", argv[0]);
@@ -44,7 +46,9 @@ int main(int argc, char **argv)
     while (1)
     {
         clientlen = sizeof(clientaddr);
+        // 연결식별자 포인터 말록, 엑셉트
         connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        // Getnameinfo : 클라이언트 hostname과 port를 채우고 accept 문자 적어줌
         Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
         printf("Accepted connection from (%s, %s)\n", hostname, port);
         sbuf_insert(&sbuf, connfd);
@@ -63,31 +67,30 @@ void* thread(void* vargp){
 }
 void doit(int fd)
 {
-    // 프록시는 get요청만 받으면 됨. 정적인지 동적인지 몰라도됨 서버입장에서 프록시는 클라이언트 임. is_static 필요X
-    // sbuf 에 메타데이터 정보가 저장됨 (필요함)
-    //
     char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], url[MAXLINE], version[MAXLINE], port[MAXLINE], host[MAXLINE], filename[MAXLINE];
     char response[MAX_OBJECT_SIZE];
     rio_t client_rio,server_rio;
 
     Rio_readinitb(&client_rio, fd);
-    Rio_readlineb(&client_rio, buf, MAXLINE);//요청 헤더를 읽음
+    Rio_readlineb(&client_rio, buf, MAXLINE);
     printf("Request headers:\n");
     printf("%s\n", buf);
     sscanf(buf, "%s %s %s", method, uri, version);
-
     strcpy(url, uri);
     parsingHeader(uri, host, port, filename);
 
+    /* GET method만 받음 */
     if(strcasecmp(method, "GET") != 0) {
         sprintf(buf, "GET요청이 아닙니다.\r\n");
         Rio_writen(fd, buf, strlen(buf));
         return;
     }
 
+    /* 클라이언트 to 프록시 서버 request 내용 받음 */
     printf("=======Receive Request From Client=======\n");
     read_requesthdrs(&client_rio);
 
+    // P(&mutex);
     /*
      * 여기서 캐시에 데이터가 있으면 리턴
      * 요청 헤더의 파싱된 값들을 통해서 캐시 블록을 insert
@@ -99,25 +102,24 @@ void doit(int fd)
         return;
     }
     printf("=======Receive Request is Not In Cache=======\n");
+
+    /* 프록시 서버 to 타겟 서버 처리*/
     int serverFd = Open_clientfd(host, port);
     Rio_readinitb(&server_rio, serverFd);
-
     printf("=======Send Request To Server=======\n");
     make_request_to_server(serverFd, uri, host, port, method, version, filename);
-
     printf("=======Receive Request From Server=======\n");
     Rio_readnb(&server_rio, response, MAX_OBJECT_SIZE);
-    printf("=======Saved Data to Cache=======\n");
-    printf("Before insert Current Cache Number : %d\n", cacheList->currentElementCount);
-    printf("Before insert Current Front : %p\n", cacheList->frontNode);
     insertCacheNode(cacheList, url, response);
-    printf("After insert Current Cache Number : %d\n", cacheList->currentElementCount);
-    printf("After insert Current Front : %p\n", cacheList->frontNode);
     printf("%s", response);
+    Close(serverFd);
+
+    // V(&mutex);
+
+    /* 클라이언트 to 프록시 서버 내용 보냄 */
     printf("=======Send Response To Client=======\n");
     Rio_writen(fd, response, MAX_OBJECT_SIZE);
     printf("%s", response);
-    Close(serverFd);
 }
 
 void parsingHeader(char* uri,char* host,char* port,char* filename){
@@ -175,8 +177,8 @@ void make_request_to_server(int ptsfd,char* url, char* host, char* port, char* m
     printf("%s", buf);
 }
 /*
-GET http://3.36.100.140:5000 HTTP/1.0
-Host:3.36.100.140:1234
+GET http://13.125.102.254:9999 HTTP/1.0
+Host:13.125.102.254:5000
 User-Agent: "Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3"
 Connection: keep
 Proxy-Connection: keep
