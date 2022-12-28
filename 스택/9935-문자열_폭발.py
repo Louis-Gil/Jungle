b75d5e1a6bb73887a855841d8934f12675281e39
Gilinsik <lotus0110@nate.com>
2022-12-28 11:56:42 +0900

    solve : 9935

---
+++ 스택/9935-문자열_폭발.py
@@ -0,0 +1,22 @@
+# https://www.acmicpc.net/problem/9935
+import sys
+
+input = sys.stdin.readline
+
+text_line = input().strip()
+boom = input().strip()
+
+last_char = boom[-1]
+result = []
+
+for char in text_line:
+    result.append(char)
+    if char == last_char and ''.join(result[-len(boom):]) == boom:
+        del result[-len(boom):]
+    
+result = ''.join(result)    
+
+if result :
+    print(result)
+else:
+    print('FRULA')
