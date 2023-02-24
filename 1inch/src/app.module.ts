ce319855c4242ca3f4010aa23309b29d82f5c3ab
Gilin3000 <lotus0110@nate.com>
2023-02-24 11:02:58 +0900

    feat : swap init

--- 1inch/src/app.module.ts
+++ 1inch/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SwapModule } from './swap/swap.module';
 
 @Module({
-  imports: [],
+  imports: [SwapModule],
   controllers: [AppController],
   providers: [AppService],
 })
