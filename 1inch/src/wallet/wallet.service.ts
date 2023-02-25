663dfb8fbf61a306596af639d02db87f99f4b456
Gilin3000 <lotus0110@nate.com>
2023-02-25 20:19:22 +0900

    feat : apiRequestUrl func

--- 1inch/src/wallet/wallet.service.ts
+++ 1inch/src/wallet/wallet.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import Web3 from 'web3';
 
 @Injectable()
 export class WalletService {
+  web3RpcUrl = 'https://rpc-mainnet.matic.network';
+  web3 = new Web3(this.web3RpcUrl);
+
+  chainId = 137;
+  apiBaseUrl = 'https://api.1inch.io/v5.0/' + this.chainId;
+
   async quote(tokenAddress, walletAddress) {
     const remain = await this.checkCoin(tokenAddress, walletAddress);
-
     return remain;
   }
 
   async swap() {
     return 'swap';
+  }
+
+  async apiRequestUrl(methodName, queryParams) {
+    return this.apiBaseUrl + methodName
+      ? new URLSearchParams(queryParams).toString()
+      : '';
   }
 
   async checkCoin(tokenAddress, walletAddress) {
