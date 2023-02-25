3d30d721a636896d19c3f93ad1a0635dce47f036
Gilin3000 <lotus0110@nate.com>
2023-02-25 16:01:11 +0900

    feat : add env

--- 1inch/src/app.module.ts
+++ 1inch/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [WalletModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), WalletModule],
   controllers: [AppController],
   providers: [AppService],
 })
