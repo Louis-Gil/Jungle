6653b8b5f6c369a1a34db627c5a99cbe9727725c
Gilin3000 <lotus0110@nate.com>
2023-02-25 16:24:22 +0900

    feat : add interceptors to main

--- 1inch/src/main.ts
+++ 1inch/src/main.ts
@@ -1,8 +1,11 @@
+import { SuccessInterceptor } from './common/interceptors/success.interceptor';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor(), new SuccessInterceptor());
   await app.listen(3000);
 }
 bootstrap();
