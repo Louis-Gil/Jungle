ee3faff39354c45c02d767355d8f3a5f3dcbe56f
Gilin3000 <lotus0110@nate.com>
2023-02-27 19:19:50 +0900

    feat : add validation pipe

--- 1inch/src/wallet/dto/wallet.dto.ts
+++ 1inch/src/wallet/dto/wallet.dto.ts
@@ -1,14 +1,36 @@
+import { IsString, IsNumber, IsNotEmpty } from 'class-validator';
+
 export class WalletRequestDto {
+  @IsNotEmpty()
+  @IsString()
   fromTokenAddress: string;
+
+  @IsNotEmpty()
+  @IsString()
   toTokenAddress: string;
+
+  @IsNotEmpty()
+  @IsString()
   walletAddress: string;
+
+  @IsNotEmpty()
+  @IsString()
   amount: string;
+
+  @IsNotEmpty()
+  @IsString()
   mode: string;
+
   slippage?: number;
   private_key?: string;
 }
 
 export class QuoteDto {
+  @IsNotEmpty()
+  @IsString()
   amount: string;
+
+  @IsNotEmpty()
+  @IsNumber()
   estimatedGas: number;
 }
--- 1inch/src/wallet/wallet.controller.ts
+++ 1inch/src/wallet/wallet.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { Body } from '@nestjs/common/decorators';
 import { WalletRequestDto } from './dto/wallet.dto';
 import { WalletService } from './wallet.service';
@@ -8,11 +14,13 @@ export class WalletController {
   constructor(private readonly walletService: WalletService) {}
 
   @Get('/quote')
+  @UsePipes(new ValidationPipe({ transform: true }))
   async Quote(@Body() walletRequestDto: WalletRequestDto) {
     return this.walletService.getQuote(walletRequestDto);
   }
 
   @Post('/swap')
+  @UsePipes(new ValidationPipe({ transform: true }))
   async Swap(@Body() walletRequestDto: WalletRequestDto) {
     return this.walletService.postSwap(walletRequestDto);
   }
