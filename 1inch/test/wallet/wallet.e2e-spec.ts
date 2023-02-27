67a07568439c5a827032dad20c3ded60a5d43679
Gilin3000 <lotus0110@nate.com>
2023-02-27 13:29:19 +0900

    fix : test - get wallet/quote

--- 1inch/test/wallet/wallet.e2e-spec.ts
+++ 1inch/test/wallet/wallet.e2e-spec.ts
@@ -24,10 +24,14 @@ describe('Wallet', () => {
   it(`/GET wallet/quote`, () => {
     return request(app.getHttpServer())
       .get('/wallet/quote')
-      .expect(200)
-      .expect({
-        message: 'Hello World!',
-      });
+      .send({
+        fromTokenAddress: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+        toTokenAddress: '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063',
+        walletAddress: '0x4acB15Eb89cC8e396FaFd1117A0c5c414c20b5C2',
+        amount: '1000000000000000000',
+        mode: 'fixed_to_amount',
+      })
+      .expect(200);
   });
 
   afterAll(async () => {
