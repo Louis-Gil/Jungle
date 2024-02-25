82a56f6363001e732bbc4797c23377c431c04bcd
Louis-Gil <lotusnate@gmail.com>
2024-02-25 23:53:54 +0900

    algo : Add bfs_dfs sol3

--- Solve_Algorithms/impl/bfs_dfs.py
+++ Solve_Algorithms/impl/bfs_dfs.py
@@ -73,3 +73,51 @@ def solution_dfs_2(n, computers):
 
 # print(solution_bfs_2(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])) # 2
 # print(solution_dfs_2(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])) # 2
+
+
+# https://school.programmers.co.kr/learn/courses/30/lessons/1844
+def solution_bfs_3(maps):
+    def bfs(start, visited):
+        queue = deque([start])
+        visited[start[0]][start[1]] = 1
+
+        dy = [0, 1, 0, -1]
+        dx = [1, 0, -1, 0]
+
+        while queue:
+            y, x = queue.popleft()
+            for i in range(4):
+                ny = y + dy[i]
+                nx = x + dx[i]
+                if 0 <= ny < len(maps) and 0 <= nx < len(maps[0]) and maps[ny][nx] == 1 and visited[ny][nx] == 0:
+                    queue.append((ny, nx))
+                    visited[ny][nx] = visited[y][x] + 1
+
+    visited = [[0] * len(maps[0]) for _ in range(len(maps))]
+    bfs((0, 0), visited)
+    if visited[-1][-1] == 0:
+        return -1
+    else:
+        return visited[-1][-1]
+
+def solution_dfs_3(maps):
+    def dfs(y, x, visited, count):
+        visited[y][x] = count
+        dy = [0, 1, 0, -1]
+        dx = [1, 0, -1, 0]
+
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < len(maps) and 0 <= nx < len(maps[0]) and maps[ny][nx] == 1 and (visited[ny][nx] == 0 or visited[ny][nx] > count + 1):
+                dfs(ny, nx, visited, count + 1)
+
+    visited = [[0] * len(maps[0]) for _ in range(len(maps))]
+    dfs(0, 0, visited, 1)
+    if visited[-1][-1] == 0:
+        return -1
+    else:
+        return visited[-1][-1]
+
+# print(solution_bfs_3([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]])) # 11
+# print(solution_dfs_3([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]])) # 11
