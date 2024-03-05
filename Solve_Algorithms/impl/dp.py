c0b1aba98f14bf158d807332f5f21e317589cab1
Louis-Gil <lotusnate@gmail.com>
2024-03-05 20:37:00 +0900

    algo : Add dp sol 5

--- Solve_Algorithms/impl/dp.py
+++ Solve_Algorithms/impl/dp.py
@@ -106,3 +106,19 @@ def solution_dp_4(arr):
     return result
 
 # print(solution_dp_4(["5", "-", "3", "+", "1", "+", "2", "-", "4"])) # 3
+
+
+def solution_dp_5(money):
+    dp1 = [0] * len(money)
+    dp1[0], dp1[1] = money[0], max(money[0], money[1])
+    for i in range(2, len(money) - 1):
+        dp1[i] = max(dp1[i-1], dp1[i-2] + money[i])
+
+    dp2 = [0] * len(money)
+    dp2[0], dp2[1] = 0, money[1]
+    for i in range(2, len(money)):
+        dp2[i] = max(dp2[i-1], dp2[i-2] + money[i])
+
+    return max(dp1[-2], dp2[-1])
+
+# print(solution_dp_5([1, 2, 3, 1])) # 4
